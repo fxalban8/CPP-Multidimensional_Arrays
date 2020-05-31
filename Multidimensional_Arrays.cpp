477d8f126c5ec2ef4a77509aa9ad0289eb6bce7a
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-31 10:07:22 -0500

    Declaring a multidimensional string array, assigning values to each element and printing them all

--- Multidimensional_Arrays.cpp
+++ Multidimensional_Arrays.cpp
@@ -13,7 +13,7 @@ int main(){
 			counter+=1;
 		}
 	}
-	
+		
 
 	//printing each element of the multidimensional array
 	for(int i=0;i<2;i++){
@@ -21,6 +21,16 @@ int main(){
 			cout<<"Elemento "<<i<<"-"<<j<<" es "<<mArray[i][j]<<endl;
 		}
 	}
+
+	cout<<"\n"<<flush;	
+
+	//declaring a multidimensional string array and assigning values
+	string sArray[][4]={{"A","B","C","D"},{"K","L","M","N"},{"V","W","X","Y"}};
+	for(int i=0;i<3;i++){
+		for(int j=0;j<4;j++){
+			cout<<"Elemento "<<i<<"-"<<j<<" es "<<sArray[i][j]<<endl;
+		}
+		}
 	
 
 	return 0;
